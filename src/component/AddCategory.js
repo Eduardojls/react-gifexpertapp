502a4729925925f5499911f76cc30bd910e1648d
Eduardojls <eduardojls224@gmail.com>
2021-12-06 15:59:06 -0400

    pruebas renovadas

--- src/component/AddCategory.js
+++ src/component/AddCategory.js
@@ -2,35 +2,38 @@ import { useState } from 'react'
 import PropTypes from 'prop-types';
 
 // Where user enter through form the gif topic they want to see<
-export const AddCategory = ( {setCategories} ) => {
+export const AddCategory = ({ setCategories }) => {
 
     // useState Hook: Store and edit the content written in the form
-    const [inputValue, setInputValue ] = useState('');
+    const [inputValue, setInputValue] = useState('');
 
     const handleInputChange = (e) => {
         setInputValue(e.target.value);
     };
 
     const handleSubmit = (e) => {
+
         e.preventDefault();
 
-        if( inputValue.trim().length > 2 ){
-            setCategories( cats => [inputValue, ...cats]);
+        if (inputValue.trim().length > 2) {
+            setCategories(cats => [inputValue, ...cats]);
+            setInputValue('');
         };
     };
 
     return (
-        <form onSubmit = {handleSubmit}>
+        <form onSubmit={handleSubmit}>
+            <p>{inputValue}</p>
             <input
-                type = "text"
-                value = {inputValue}
-                onChange = {handleInputChange}
+                type="text"
+                value={inputValue}
+                onChange={handleInputChange}
             />
         </form>
     );
 };
 
 AddCategory.propTypes = {
-    setCategories: PropTypes.func.isRequired       
+    setCategories: PropTypes.func.isRequired
 };
 
--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -4,14 +4,54 @@ import { AddCategory } from '../../component/AddCategory';
 
 describe('Pruebas en <AddCategory />', () => {
 
-    const setCategories = () => {};
-    
-    test('debe de mostrar el componente correctamente', () => {
-        
-        
+    const setCategories = jest.fn();
+    let wrapper = shallow(<AddCategory setCategories={setCategories} />);
 
-        const wrapper = shallow( <AddCategory setCategories = { setCategories }/> );
-        expect( wrapper ).toMatchSnapshot();
+    beforeEach(() => {
+        jest.clearAllMocks();
+        wrapper = shallow(<AddCategory setCategories={setCategories} />);
     });
- 
+
+    test('debe de mostrar el componente correctamente', () => {
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('debe de cambiar la caja de texto', () => {
+
+        const input = wrapper.find('input');
+        const value = 'Hola Mundo';
+
+        input.simulate('change', { target: { value } });
+        const p = wrapper.find('p');
+        expect(p.text().trim()).toBe(value);
+    });
+
+    test('NO debe de postear la información con submit', () => {
+
+        const preventDefault = () => { };
+        const form = wrapper.find('form');
+
+        form.simulate('submit', { preventDefault });
+        expect(setCategories).not.toHaveBeenCalled();
+    });
+
+    test('debe de llamar el setCategories y limpiar la caja de texto', () => {
+
+        const value = 'Probando';
+
+        // 1. simular el handleInputChange
+        wrapper.find('input').simulate('change', { target: { value } });
+
+        // 2. simular el submit
+        wrapper.find('form').simulate('submit', { preventDefault() { } });
+
+        // 3. setCategories se debe haber llamado 1 vez
+        expect(setCategories).toHaveBeenCalled();
+
+        // 4. El valor del input debe estar ''
+        expect(wrapper.find('input').prop('value')).toBe('');
+    });
+
+
 });
